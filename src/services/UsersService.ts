b4ac0f44ca2a68b2d17698d744205e4641a21454
Bruno Salviano da Silva Teixeira <br_salviano@hotmail.com>
2021-04-21 16:30:08 -0300

    Dia 3: Parte de mensagens com relacionamento entre tabelas, camada de serviços e outras refatorações

---
+++ src/services/UsersService.ts
@@ -0,0 +1,35 @@
+import { getCustomRepository, Repository } from "typeorm"
+import { User } from "../entities/User"
+import { UsersRepository } from "../repositories/UsersRepository"
+
+class UsersService {
+
+    private usersRepository: Repository<User>
+
+    constructor() {
+        this.usersRepository = getCustomRepository(UsersRepository)
+    }
+
+    async create(email: string) {
+
+        //verificar se usuário existe
+        const userExists = await this.usersRepository.findOne({
+            email
+        })
+        //se existir, retornar user
+        if(userExists) {
+            return userExists
+        }
+
+        //se não existir, salvar no db e retorna o usuário criado
+        const user = this.usersRepository.create({
+            email
+        })
+        await this.usersRepository.save(user)
+
+        return user
+
+    }
+}
+
+export { UsersService }
